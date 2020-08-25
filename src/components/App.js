94b46243be9565c51728eccc95604d608501eab8
Viraj Nimbalkar <virajnimbalkar55@gmail.com>
2020-08-25 13:09:22 +0530

    Add files via upload

---
+++ src/components/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import TaskList from './TaskList';
+import TaskForm from './TaskForm';
+import Header from './Header';
+import TaskListContextProvider from '../context/TaskListContext'
+import "../App.css";
+
+const App = () => {
+  return(
+
+    <TaskListContextProvider>
+
+      <div className="container">
+        <div className="app-wrapper">
+          <Header/>
+
+          <div className="main">
+              <TaskForm/>
+              <TaskList/>
+          </div>
+          <div className="github"> <a href="https://github.com/Viraaaj/My-Tasks" target="_blanck">My-Tasks/Viraj</a> </div>
+        </div>
+        
+      </div>
+
+      
+
+
+    </TaskListContextProvider>
+    
+  ) 
+}
+
+export default App
---
+++ src/components/TaskForm.js
@@ -0,0 +1,50 @@
+import React, {useContext, useState, useEffect} from 'react'
+import { TaskListContext } from '../context/TaskListContext'
+
+const TaskForm = () => {
+  const {addTask, clearList, editItem, editTask} = useContext(TaskListContext)
+
+  const [title, setTitle] = useState('')
+
+  const handleChange = e => {
+    setTitle(e.target.value)
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    if(!editItem) {
+      addTask(title)
+      setTitle('')
+    } else{
+      editTask(title, editItem.id)
+    }
+  };
+
+  useEffect(() => {
+    if(editItem) {
+      setTitle(editItem.title)
+    } else{
+      setTitle('');
+    }
+  }, [editItem]);
+
+
+  return (
+    <form onSubmit={handleSubmit} className="form">
+
+      <input onChange={handleChange} 
+      value={title} 
+      type="text" 
+      className="task-input" 
+      placeholder="Add Task..." required/>
+
+      <div className="buttons">
+        <button type="submit" className="btn add-task-btn">{editItem ? 'Edit Task' : 'Add Task'}</button>
+
+        <button onClick={clearList} className="btn clear-btn">Clear</button>
+      </div>
+    </form>
+  )
+}
+
+export default TaskForm
